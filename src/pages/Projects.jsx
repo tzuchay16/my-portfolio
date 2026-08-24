0d24d052c774db419982aa1036cad2b90b390b6f
tzuchay16 <tzuchay16@gmail.com>
2026-08-24 08:38:38 +0800

    update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
-import { projects } from '../data/projects';
+import { Link } from "react-router-dom";
+import { projects } from "../data/projects";
 
 export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <div>
+      <div className="project-list">
         {projects.map((project) => (
-          <div key={project.id}>
+          <div className="project-card" key={project.id}>
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <Link to={`/projects/${project.id}`}>View Details</Link>
